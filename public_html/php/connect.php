0b54af60851869b6c7b2bd4a132f5dc2c1bed172
MiPc <MiPc@10.0.0.113>
2020-06-13 16:29:36 -0500

    Merge origin/master
    Conflicts:
    	public_html/index.html

--- public_html/php/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "id12095050_roma";      // id12095050_roma 
-    $password = "Joricopter-3333";              //Joricopter-3333
-    $database = "id12095050_articlesdstuds";    //id12095050_articlesdstuds
-?>
